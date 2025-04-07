f143769229dcd9629f42e2d2aaeb933459002097
María García <mariav2601@gmail.com>
2025-04-07 10:48:35 -0400

    Agregar página de inicio de sesión

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login - Gestor de Compras</title>
+</head>
+<body>
+    <h2>Iniciar sesión</h2>
+    <form action="login.php" method="post">
+        <label>Usuario:</label>
+        <input type="text" name="username" required><br><br>
+
+        <label>Contraseña:</label>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Ingresar</button>
+    </form>
+</body>
+</html>
